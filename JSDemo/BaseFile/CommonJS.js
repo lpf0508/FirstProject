7f909ce905540b9120e390f085fae78813cca534
lpf0508 <153563568@qq.com>
2015-11-19 15:03:59 +0800

    我创建

---
+++ JSDemo/BaseFile/CommonJS.js
@@ -0,0 +1,56 @@
+﻿var BaseClass = function () {
+
+
+}
+
+
+var StaticPrivate = (function () {
+    var base = function () {
+
+    }
+    base.a = "a params";
+    base.show = function () {
+        alert(base.a);
+    }
+    base.prototype.reShow = function () {
+        //alert(base.a);
+    }
+    return base;
+})();
+
+(function () {
+    if (!window.Common) {
+        window.Common = {};
+    }
+    window.Common = {
+        show: function () {
+
+        },
+        reShow: function () {
+
+        }
+    }
+})();
+
+
+var Win = function () {
+    this.init = {
+        width: '300px',
+        height:'500px'
+    },
+    this.WinObj = function () {
+        var Window = {
+            width: this.init.width,
+            height:this.init.height
+        };
+        return Window;
+    }
+}
+
+var GO = {};
+(function (o) {
+    o.add = function(){
+        return "Add Method";
+    }
+})(GO);
+
